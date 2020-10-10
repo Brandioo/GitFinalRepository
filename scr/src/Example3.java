a56c16c3ed33a63be36fcca3de3af01cb7573a76
Brandio7 <citozibrand@gmail.com>
2020-10-10 13:15:08 +0200

    commited

---
+++ scr/src/Example3.java
@@ -0,0 +1,11 @@
+public class Example3 {
+    public static void main(String[] args)
+    {
+        String s1;
+        s1="Java";
+        StringBuilder sb=new StringBuilder();
+        sb.append(s1);
+        sb.reverse();
+        System.out.println(sb.toString());
+    }
+}
