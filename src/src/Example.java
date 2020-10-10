cafdfc506cc80c9b8528214c2ca3b852aa5d9f39
Brandio7 <citozibrand@gmail.com>
2020-10-10 13:48:54 +0200

    First Commit

---
+++ src/src/Example.java
@@ -0,0 +1,19 @@
+public class Example {
+
+        public static void main(String[] args)
+        {
+            String s1;
+            s1="Java";
+            StringBuilder sb=new StringBuilder();
+            sb.append(s1);
+            sb.reverse();
+            System.out.println(sb.toString());
+
+            String s2;
+            s2="Programmer";
+            String result=s2.substring(0,5);
+            System.out.println(result);
+        }
+    }
+
+
