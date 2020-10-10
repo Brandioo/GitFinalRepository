e30baeb7852e539203aeea4181b7cf560f7f0703
Brandio7 <citozibrand@gmail.com>
2020-10-10 15:34:56 +0200

    Good

--- src/src/Example.java
+++ src/src/Example.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Example {
 
         public static void main(String []args)
@@ -14,6 +16,22 @@ public class Example {
             String result=s2.substring(0,5);
             System.out.println(result);
             System.out.println(s2.length());
+
+            //Example 8
+            System.out.println("Example 8");
+            String text1="This is a comparisson";
+            String text2="This is something";
+            System.out.println(text1.equalsIgnoreCase(text2));
+
+            //Ex-9
+            System.out.println("--------------Example 9:-------------------");
+            String text3="Java exercises";
+            String s;
+            Scanner sc=new Scanner(System.in);
+            s=sc.nextLine();
+            System.out.println(text3.endsWith(s));
+
+
         }
     }
 
